7784a30fac57c5e837e0eaf40dbd2ba393395fd3
happyKale <syk767@gmail.com>
2022-05-23 15:56:17 +0900

    Feat: axios 사용해서 http-client 기본 설정

--- src/app/libs/http-client/index.ts
+++ src/app/libs/http-client/index.ts
@@ -1,1 +1,31 @@
-export const httpClient = () => {};
+import axios from "axios";
+
+const API_ENDPOINT =
+  "https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment";
+
+export const httpClient = (() => {
+  const instance = axios.create({
+    // 기본적으로 우리가 바라볼 서버의 주소
+    baseURL: API_ENDPOINT,
+  });
+
+  // request interceptors
+  axios.interceptors.request.use((config: any) => {
+    config.headers["Content-Type"] = "application/json; charset=utf-8";
+    config.headers["X-Requested-With"] = "XMLHttpRequest";
+    config.headers.Accept = "application/json";
+    return config;
+  });
+
+  return {
+    async get<T>(url: string): Promise<T> {
+      return await instance.get(url);
+    },
+    async post<T>(
+      url: string,
+      data: Record<string, string | number>
+    ): Promise<T> {
+      return await instance.post(url, data);
+    },
+  };
+})();
