c468644817189e4e3ea9212ddf3443d9b426f029
happyKale <syk767@gmail.com>
2022-05-23 16:07:55 +0900

    Feat: 홈화면에 브랜드, 색깔, 카테고리 출력

--- src/app/screens/Home/HomeScreen.tsx
+++ src/app/screens/Home/HomeScreen.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { productRepository } from "../../repositories";
 
 function HomeScreen() {
-  return <div></div>;
+  // state
+  const [data, setData] = useState<{
+    brand: Array<{ name: string }>;
+    color: Array<{ name: string }>;
+    category: Array<{ name: string; id: number; parent_id: null }>;
+  }>({ brand: [], color: [], category: [] });
+
+  // effect
+  useEffect(() => {
+    handleGetData();
+  }, []);
+
+  // handler
+  const handleGetData = async () => {
+    const brand = await productRepository.getBrand().then((res: any) => {
+      return res.data;
+    });
+    const color = await productRepository
+      .getColor()
+      .then((res: any) => res.data);
+    const category = await productRepository
+      .getCategory()
+      .then((res: any) => res.data);
+    setData({ brand, color, category });
+  };
+
+  return (
+    <div>
+      홈
+      <div>
+        <div>
+          브랜드
+          {data.brand.map((item: { name: string }, idx) => {
+            return <div key={idx}>{item.name}</div>;
+          })}
+        </div>
+        <div>
+          색
+          {data.color.map((item: { name: string }, idx) => {
+            return <div key={idx}>{item.name}</div>;
+          })}
+        </div>
+        <div>
+          카테고리
+          {data.category.map(
+            (item: { name: string; id: number; parent_id: null }, idx) => {
+              return <div key={idx}>{item.name}</div>;
+            }
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { HomeScreen };
